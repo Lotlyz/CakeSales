87f64006224d4259a807456945f71d8bce2906d2
chuxiaxunfeng <727257386@qq.com>
2022-09-23 10:35:19 +0800

    更改配置文件

--- src/main/java/cn/lotlyz/cake/mapper/UserMapper.java
+++ src/main/java/cn/lotlyz/cake/mapper/UserMapper.java
@@ -1,4 +1,11 @@
 package cn.lotlyz.cake.mapper;
 
+import cn.lotlyz.cake.model.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 public interface UserMapper {
+    @Select("select * from user")
+    List<User> findAll();
 }
